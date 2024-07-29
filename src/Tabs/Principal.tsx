b822c524da80d0517ee3d349cd85a8ff2d7571e5
luhoro <luisashorowitz@gmail.com>
2024-07-29 12:19:14 -0300

    Feat: Home page

--- src/Tabs/Principal.tsx
+++ src/Tabs/Principal.tsx
@@ -1,10 +1,61 @@
-import { Text, VStack } from "native-base"
+import { VStack, Text, Image, Box, ScrollView, Divider } from "native-base"
+import Logo from "../assets/logo.png"
+import Botao from "../componentes/Botao"
+import EntradaTexto from "../componentes/EntradaTexto"
+import Titulo from "../componentes/Titulo"
+import Depoimentos from "../utils/mock"
 
 const Principal = () => {
   return (
-    <VStack>
-      <Text>Principal</Text>
-    </VStack>
+    <ScrollView flex={1} bgColor={"white"}>
+      <VStack
+        flex={1}
+        alignItems={"flex-start"}
+        justifyContent={"flex-start"}
+        p={5}
+      >
+        <Image source={Logo} alt="Logo" mt={10} />
+        <Titulo color={"blue.500"}>Boas vindas!</Titulo>
+
+        <Box
+          w={"100%"}
+          borderRadius={"lg"}
+          p={3}
+          mt={10}
+          shadow={"1"}
+          borderRightRadius={"md"}
+        >
+          <EntradaTexto placeholder="Digite a especialidade" type="text" />
+          <EntradaTexto placeholder="Digite sua localização" type="text" />
+
+          <Botao mt={3} mb={3}>
+            Buscar
+          </Botao>
+        </Box>
+
+        <Titulo color={"blue.800"} alignSelf={"center"} mt={4}>
+          Depoimentos
+        </Titulo>
+        <VStack space={3} divider={<Divider />} w={"100%"}>
+          {Depoimentos.map((depoimento) => (
+            <Box key={depoimento.id} w={"100%"} borderRadius={"lg"} p={3}>
+              <Text color={"gray.300"} fontSize={"md"} textAlign={"justify"}>
+                {depoimento.text}{" "}
+              </Text>
+
+              <Text
+                color={"gray.500"}
+                fontSize={"lg"}
+                fontWeight={"bold"}
+                mt={2}
+              >
+                {depoimento.titulo}
+              </Text>
+            </Box>
+          ))}
+        </VStack>
+      </VStack>
+    </ScrollView>
   )
 }
 
---
+++ src/utils/mock.ts
@@ -0,0 +1,19 @@
+const Depoimentos = [
+  {
+    id: 1,
+    titulo: "Marcos, 32 anos, Contagem/MG",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis quod nemo dolore debitis, nisi quidem veniam? Rerum earum harum cumque? Amet, commodi praesentium! Aperiam sit id iste ducimus error numquam.",
+  },
+  {
+    id: 2,
+    titulo: "Henrique, 24 anos, Ipatinga/MG",
+    text: "Donec eleifend ipsum a est viverra interdum. Nulla a orci arcu. Duis ullamcorper gravida feugiat. Duis rutrum suscipit lorem. Nulla facilisis, diam eget sagittis volutpat, nulla neque rutrum turpis, a convallis est dolor sit amet velit. Praesent eleifend ultricies rhoncus. Curabitur consectetur sem at mi ullamcorper, ac sollicitudin leo accumsan. Curabitur molestie tincidunt augue, eget dapibus massa lacinia in. Duis ut ornare lorem.",
+  },
+  {
+    id: 3,
+    titulo: "Patrícia, 35 anos, Uberaba/MG",
+    text: "Morbi vestibulum tincidunt tellus, ut tempus nulla euismod ac. Sed sagittis cursus tortor, sit amet scelerisque elit finibus vitae. Cras egestas volutpat purus ut tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed eleifend consectetur efficitur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+  },
+]
+
+export default Depoimentos
