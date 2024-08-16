0780247a5b6b741223dfd9a6c87d7d875e441384
luhoro <luisashorowitz@gmail.com>
2024-08-16 10:21:42 -0300

    Feat: Login functionality

--- src/Login.tsx
+++ src/Login.tsx
@@ -3,18 +3,36 @@ import {
   Text,
   VStack,
   Box,
-  FormControl,
-  Input,
-  Button,
   Link,
+  useToast
 } from "native-base"
 import { TouchableOpacity } from "react-native"
 import Logo from "./assets/logo.png"
 import Titulo from "./componentes/Titulo"
 import EntradaTexto from "./componentes/EntradaTexto"
 import Botao from "./componentes/Botao"
+import { useState } from "react"
+import { fazerLogin } from "./servicos/AutenticacaoServico"
 
-export const Login = ({ navigation }) => {
+export const Login = ({ navigation }: any) => {
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const toast = useToast()
+
+  const login = async () => {
+    const resultado = await fazerLogin(email, senha)
+
+    if (resultado) {
+      navigation.replace("Tabs")
+    } else {
+      toast.show({
+        title: "Erro no login",
+        description: "o email ou senha não conferem",
+        backgroundColor: "red.500"
+      })
+    }
+  }
+
   return (
     <VStack flex={1} alignItems={"center"} p={5} justifyContent={"center"}>
       <Image source={Logo} alt="Logo Voll" />
@@ -25,18 +43,19 @@ export const Login = ({ navigation }) => {
         <EntradaTexto
           label="E-mail"
           placeholder="Insira seu endereço de e-mail"
-          type="text"
+          value={email}
+          onChangeText={setEmail}
         />
 
         <EntradaTexto
           label="Senha"
           placeholder="Insira sua senha"
-          type="password"
+          value={senha}
+          onChangeText={setSenha}
         />
       </Box>
 
-      <Botao onPress={()=> navigation.navigate('Tabs')
-      }>Entrar</Botao>
+      <Botao onPress={login}>Entrar</Botao>
 
       <Link href="https://google.com" mt={2}>
         Esqueceu sua senha?
