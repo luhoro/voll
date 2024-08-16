0a6eb8bafcac9634484e08d143401f96a0d53a60
luhoro <luisashorowitz@gmail.com>
2024-08-16 10:50:33 -0300

    Feat: Automatic login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,37 +1,56 @@
-import {
-  Image,
-  Text,
-  VStack,
-  Box,
-  Link,
-  useToast
-} from "native-base"
+import { Image, Text, VStack, Box, Link, useToast } from "native-base"
 import { TouchableOpacity } from "react-native"
 import Logo from "./assets/logo.png"
 import Titulo from "./componentes/Titulo"
 import EntradaTexto from "./componentes/EntradaTexto"
 import Botao from "./componentes/Botao"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { fazerLogin } from "./servicos/AutenticacaoServico"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { jwtDecode } from "jwt-decode"
 
 export const Login = ({ navigation }: any) => {
   const [email, setEmail] = useState("")
   const [senha, setSenha] = useState("")
+  const [carregando, setCarregando] = useState(true)
   const toast = useToast()
+
+  useEffect(() => {
+    // AsyncStorage.removeItem('token')
+
+    const verificarLogin = async () => {
+      const token = await AsyncStorage.getItem("token")
+
+      if (token) navigation.replace("Tabs")
+
+      setCarregando(false)
+    }
+    verificarLogin()
+  }, [])
 
   const login = async () => {
     const resultado = await fazerLogin(email, senha)
 
     if (resultado) {
+      const { token } = resultado
+      AsyncStorage.setItem("token", token)
+
+      const tokenDecodificado = jwtDecode(token) as any
+      const pacienteId = tokenDecodificado.id
+
+      AsyncStorage.setItem("pacienteId", pacienteId)
+
       navigation.replace("Tabs")
     } else {
       toast.show({
         title: "Erro no login",
         description: "o email ou senha não conferem",
-        backgroundColor: "red.500"
+        backgroundColor: "red.500",
       })
     }
   }
+
+  if (carregando) return null
 
   return (
     <VStack flex={1} alignItems={"center"} p={5} justifyContent={"center"}>
